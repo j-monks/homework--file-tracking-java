2e5c0d66eb05ef33e9f4995bd622b67f1982f129
James Monks <J.monks23@gmail.com>
2020-09-08 17:44:11 +0100

    set up file and folder models

---
+++ src/main/java/com/codeclan/example/fileservice/models/Folder.java
@@ -0,0 +1,48 @@
+package com.codeclan.example.fileservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonIgnoreProperties({"folders"})
+    @OneToMany(mappedBy = "folder")
+    private List<File> files;
+
+    public Folder(String title) {
+        this.title = title;
+        this.files = new ArrayList<>();
+    }
+
+    public Folder() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
---
+++ src/main/java/com/codeclan/example/fileservice/models/User.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.fileservice.models;
+
+public class User {
+}
